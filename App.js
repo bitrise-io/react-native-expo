837ee4f6caeed969bdf3eae09d88ff72587e7de5
Akos Birmacher <akos.birmacher@bitrise.io>
2018-09-11 16:43:13 +0200

    dependency: add contacts expo dependency

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Contacts } from 'expo';
 
 export default class App extends React.Component {
   render() {
